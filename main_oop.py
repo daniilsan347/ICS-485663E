b5739cfc72465e76616b9d1b1a6b53781baf4998
daniilsan347 <daniilsan347@gmail.com>
2021-11-05 21:11:48 +0200

    Tables in CSV

---
+++ main_oop.py
@@ -0,0 +1,17 @@
+# ICS-285663 - OOP Method
+
+import csv
+import sys
+
+
+class Product:
+    objectID = None
+    productID = None
+    year = None
+    name = None
+    basePrice = None
+    marketPrice = []
+    marketPriceAvg = None
+
+    def __init__(self) -> None:
+        pass
